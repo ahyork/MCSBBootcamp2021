b194103dbb4cf284e65dfdee0da5fe9fad1b3716
Arthur York <ahyork98@gmail.com>
2021-08-25 09:47:18 -0700

    finished working on the parametrized julia function

--- Module2/project1.m
+++ Module2/project1.m
@@ -4,37 +4,67 @@
 % x - fraction of caffeinated 
 
 size = 22; % max number of days to simulate
-c = -0.8    % given constant for the equation
-d = 0.156   % given constant for the equation
+c = -0.8;    % given constant for the equation
+d = 0.156;   % given constant for the equation
 
-% define arrays for storing the information
-x = zeros(1, size)
-y = zeros(1, size)
-% set starting conditions for the arrays
-x(1) = 0.1
-y(1) = 0.1
+NStartingPoints = 1e5;  % define the number of random starting points we use
+min_start = -2;
+max_start = 2;
 
-for n=2:size
-    x(n) = x(n - 1) ^ 2 - y(n - 1) ^ 2 + c
-    y(n) = 2 * x(n - 1) * y(n - 1) + d
-end % finished loop through days
+x_starts = (max_start - min_start) * rand(1, NStartingPoints) .+ min_start
+y_starts = (max_start - min_start) * rand(1, NStartingPoints) .+ min_start
+
+figure(1);
+plot(x_starts, y_starts, 'ok')
+ylabel('y initials')
+xlabel('x initials')
+pause()
+
+within_bounds = false(1, NStartingPoints)
+
+for i = 1:NStartingPoints
+    % define arrays for storing the information
+    x = zeros(1, size)
+    y = zeros(1, size)
+    % set starting conditions for the arrays
+    x(1) = x_starts(i)
+    y(1) = y_starts(i)
+
+    for n=2:size
+        x(n) = x(n - 1) ^ 2 - y(n - 1) ^ 2 + c;
+        y(n) = 2 * x(n - 1) * y(n - 1) + d;
+    end % finished loop through days
+
+    within_bounds(i) = abs(x(22)) < 2 && abs(y(22)) < 2;
+end % loop through different starting points
 
 % THE MODEL ^
 % ------------------------------------------
 % THE BEHAVIOR / THE OUTPUT ? 
 
-% x(n) and y(n) versus n
-figure(1); 
-plot(x,'-or');
+% plot the starting points with colors to show if they converge or not
+figure(2);
+plot(x_starts(within_bounds), y_starts(within_bounds), 'ob')
 hold on
-plot(y,'-ob');
+plot(x_starts(!within_bounds), y_starts(!within_bounds), 'or')
 hold off
-ylabel('output')
-xlabel('n')
+ylabel('y initials')
+xlabel('x initials')
 pause()
 
-figure(2);
-plot(x, y, '-ok')
-ylabel('y(n)')
-xlabel('x(n)')
-pause();
+% x(n) and y(n) versus n
+%figure(3); 
+%plot(x,'-or');
+%hold on
+%plot(y,'-ob');
+%hold off
+%ylabel('output')
+%xlabel('n')
+%pause()
+
+% y(n) v x(n)
+%figure(4);
+%plot(x, y, '-ok')
+%ylabel('y(n)')
+%xlabel('x(n)')
+%pause();
