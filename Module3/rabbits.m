44513d9736a13a646198d3abe0e325a5372bea89
Arthur York <ahyork98@gmail.com>
2021-08-26 15:20:07 -0700

    finished rabbits.m, created bifurcation diagram

--- Module3/rabbits.m
+++ Module3/rabbits.m
@@ -1,19 +1,33 @@
 % define constants for use in the rabbit population growth equation
-r = 2.1;         % the growth rate
+r = 2.35;         % the growth rate
 K = 0.6;         % the carrying capacity
 starting_pop = 0.2;      % the starting population for the rabbits
 
-num_time_steps = 50;
-
-x = zeros(1, num_time_steps);
-x(1) = starting_pop;
-
-for t = 1:num_time_steps
-    x(t + 1) = x(t) + r * (1 - x(t) / K) * x(t);
-end
+n_mid = 30;
+n_max = n_mid * 2;
+n_range = n_max-n_mid:n_max
 
 figure(1);
-plot(x, '-ok')
+
+for r = 0.1:0.01:3.0
+
+    x = zeros(1, n_max);
+    x(1) = starting_pop;
+
+    for t = 1:n_max
+        x(t + 1) = x(t) + r * (1 - x(t) / K) * x(t);
+    end
+
+    plot(zeros(1, length(n_range)) + r, x(n_range), '.k')
+    %plot(x(n_range), '.k')
+    %plot(x, '-ok')
+    hold on
+
+end
+
+hold off
+
 ylabel('Population (thousands)')
 xlabel('time (months)')
 pause()
+display(x)
